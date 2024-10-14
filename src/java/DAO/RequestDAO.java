b5d7e8707ef08623ede79c23ac8b7246fa9138c5
hoanganh <nhhag@valerian>
2024-10-14 16:12:10 +0700

    Fix request

--- src/java/DAO/RequestDAO.java
+++ src/java/DAO/RequestDAO.java
@@ -52,25 +52,24 @@ public class RequestDAO extends DBContext {
     }
 
     public void insertRequest(Request request) {
-        String sql = "INSERT INTO Request (MentorID, MenteeID, Price, Note, CreateDate, Status, Title, DeadlineHour, DeadlineDate, Framework, StartDate, EndDate) "
+        String sql = "INSERT INTO Request (MentorID, MenteeID, Price, Note, CreateDate, Status, Title,Framework, StartDate, EndDate, SkillID) "
                 + "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
         try {
             PreparedStatement st = connection.prepareStatement(sql);
 
             // Set parameters for the query
-            st.setObject(1, request.getMentorId(), java.sql.Types.INTEGER);
-            st.setObject(2, request.getMenteeId(), java.sql.Types.INTEGER);
+            st.setInt(1, request.getMentorId());
+            st.setInt(2, request.getMenteeId());
             st.setFloat(3, request.getPrice());
             st.setString(4, request.getNote());
             st.setDate(5, Date.valueOf(request.getCreateDate()));
             st.setString(6, request.getStatus());
             st.setString(7, request.getTitle());
-            st.setTime(8, Time.valueOf(request.getDeadlineHour()));
-            st.setDate(9, Date.valueOf(request.getDeadlineDate()));
-            st.setString(10, request.getFramework());
-            st.setDate(11, Date.valueOf(request.getStartDate()));
-            st.setDate(12, Date.valueOf(request.getEndDate()));
+            st.setString(8, request.getFramework());
+            st.setDate(9, Date.valueOf(request.getStartDate()));
+            st.setDate(10, Date.valueOf(request.getEndDate()));
+            st.setInt(11, request.getSkillId());
             st.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
@@ -78,10 +77,9 @@ public class RequestDAO extends DBContext {
 
     }
 
-    public void addItemByRequestID(String[] skills, String[] slots) {
+    public void addItemByRequestID(String[] slots) {
         String sql1 = "SELECT TOP 1 RequestID FROM [dbo].[Request] ORDER BY RequestID DESC";
-        String sql2 = "INSERT INTO [dbo].[RequestItem] (RequestID, SkillID) VALUES (?, ?)";
-        String sql3 = "INSERT INTO [dbo].[RequestSlotItem] (RequestID, SlotID) VALUES (?, ?)";
+        String sql2 = "INSERT INTO [dbo].[RequestSlotItem] (RequestID, SlotID) VALUES (?, ?)";
         try {
             PreparedStatement st = connection.prepareStatement(sql1);
             ResultSet rs = st.executeQuery();
@@ -91,14 +89,6 @@ public class RequestDAO extends DBContext {
             }
 
             st = connection.prepareStatement(sql2);
-            for (String skillIdStr : skills) {
-                int skillId = Integer.parseInt(skillIdStr);
-                st.setInt(1, lastRequestId);
-                st.setInt(2, skillId);
-                st.executeUpdate();
-            }
-
-            st = connection.prepareStatement(sql3);
             for (String slotIdStr : slots) {
                 int slotId = Integer.parseInt(slotIdStr);
                 st.setInt(1, lastRequestId);
@@ -131,16 +121,12 @@ public class RequestDAO extends DBContext {
                 curRequest.setCreateDate(curCreaDate);
                 curRequest.setStatus(rs.getString("Status"));
                 curRequest.setTitle(rs.getString("Title"));
-                LocalDate curDeaDate = rs.getDate("DeadlineDate").toLocalDate();
                 LocalDate start = rs.getDate("StartDate").toLocalDate();
                 LocalDate end = rs.getDate("EndDate").toLocalDate();
-                curRequest.setDeadlineDate(curDeaDate);
                 curRequest.setStartDate(start);
                 curRequest.setEndDate(end);
-                Time time = rs.getTime("DeadlineHour");
-                if (time != null) {
-                    curRequest.setDeadlineHour(time.toLocalTime());
-                }
+                curRequest.setSkillId(rs.getInt("SkillID"));
+                curRequest.setPrice(rs.getFloat("Price"));
                 curRequest.setFramework(rs.getString("Framework"));
                 listRequest.add(curRequest);
             }
@@ -169,14 +155,12 @@ public class RequestDAO extends DBContext {
                 curRequest.setCreateDate(curCreaDate);
                 curRequest.setStatus(rs.getString("Status"));
                 curRequest.setTitle(rs.getString("Title"));
-                LocalDate curDeaDate = rs.getDate("DeadlineDate").toLocalDate();
                 LocalDate start = rs.getDate("StartDate").toLocalDate();
                 LocalDate end = rs.getDate("EndDate").toLocalDate();
-                curRequest.setDeadlineDate(curDeaDate);
                 curRequest.setStartDate(start);
                 curRequest.setEndDate(end);
-                Time time = rs.getTime("DeadlineHour");
-                curRequest.setDeadlineHour(time.toLocalTime());
+                curRequest.setSkillId(rs.getInt("SkillID"));
+                curRequest.setPrice(rs.getFloat("Price"));
                 curRequest.setFramework(rs.getString("Framework"));
                 listRequest.add(curRequest);
             }
@@ -190,8 +174,7 @@ public class RequestDAO extends DBContext {
     public static void main(String[] args) {
         RequestDAO act = new RequestDAO();
         List<RequestSlotItem> curList = act.getDuplicateSlot(4);
-        for(int i = 0; i < curList.size() ; i++)
-        {
+        for (int i = 0; i < curList.size(); i++) {
             System.out.println(curList.get(i));
         }
     }
