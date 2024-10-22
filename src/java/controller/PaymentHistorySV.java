4c76bcd02449a889b8175471568922a9e461a192
Mai Tuong Huy <huymthe181917@fpt.edu.vn>
2024-10-22 14:53:34 +0700

    My 50th commit

--- src/java/controller/PaymentHistorySV.java
+++ src/java/controller/PaymentHistorySV.java
@@ -4,18 +4,10 @@ package controller;
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-<<<<<<<< HEAD:src/java/controller/Admin/DeleteSkillServlet.java
-
-package controller.Admin;
-
-import DAO.SkillDAO;
-import Model.Skill;
-========
 import DAO.PaymentDAO;
 import DAO.WalletDAO;
 import Model.Payment;
 import Model.User;
->>>>>>>> d98d727e978c449005eea11b7c2ed8e3a383f121:src/java/controller/PaymentHistorySV.java
 import java.io.IOException;
 import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
@@ -23,87 +15,53 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-<<<<<<<< HEAD:src/java/controller/Admin/DeleteSkillServlet.java
-========
 import jakarta.servlet.http.HttpSession;
 import java.util.ArrayList;
->>>>>>>> d98d727e978c449005eea11b7c2ed8e3a383f121:src/java/controller/PaymentHistorySV.java
 import java.util.List;
 
 /**
  *
- * @author tuong
+ * @author nhhag
  */
-@WebServlet(name="DeleteSkillServlet", urlPatterns={"/deleteSkillAdmin"})
-public class DeleteSkillServlet extends HttpServlet {
-   
-    /** 
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+@WebServlet(urlPatterns = {"/paymentHistory"})
+public class PaymentHistorySV extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet DeleteSkillServlet</title>");  
+            out.println("<title>Servlet PaymentHistorySV</title>");
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet DeleteSkillServlet at " + request.getContextPath () + "</h1>");
+            out.println("<h1>Servlet PaymentHistorySV at " + request.getContextPath() + "</h1>");
             out.println("</body>");
             out.println("</html>");
         }
-    } 
+    }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /** 
+    /**
      * Handles the HTTP <code>GET</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
     @Override
-<<<<<<<< HEAD:src/java/controller/Admin/DeleteSkillServlet.java
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        SkillDAO act = new SkillDAO();
-        List<Skill> listAllSkill = act.getListOfAllSkill();
-        String page_raw = request.getParameter("page");
-        String numDis_raw = request.getParameter("numDis");
-        String deleteID_raw = request.getParameter("id");
-        int deleteId = Integer.parseInt(deleteID_raw);
-        act.deleteSkillById(deleteId);
-        int page, numDis;
-        if (page_raw != null) {
-            page = Integer.parseInt(page_raw);
-        } else {
-            page = 1;
-        }
-        if (numDis_raw != null) {
-            numDis = Integer.parseInt(numDis_raw);
-        } else {
-            numDis = 5;
-        }
-        int numSkill = listAllSkill.size();
-        int numOfPage = (numSkill % numDis == 0 ? numSkill / numDis : (numSkill / numDis + 1));
-        request.setAttribute("numOfPage", numOfPage);
-        listAllSkill = act.getListOfSkillPaging(page, numDis);
-        request.setAttribute("indexPage", page);
-        request.setAttribute("numDis", numDis);
-
-        request.setAttribute("list", listAllSkill);
-        request.getRequestDispatcher("viewSkill.jsp").forward(request, response);
-    }
-
-    /** 
-========
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         HttpSession session = request.getSession();
         WalletDAO walletDAO = new WalletDAO();
@@ -147,8 +105,8 @@ public class DeleteSkillServlet extends HttpServlet {
     }
 
     /**
->>>>>>>> d98d727e978c449005eea11b7c2ed8e3a383f121:src/java/controller/PaymentHistorySV.java
      * Handles the HTTP <code>POST</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -156,12 +114,13 @@ public class DeleteSkillServlet extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    /** 
+    /**
      * Returns a short description of the servlet.
+     *
      * @return a String containing servlet description
      */
     @Override
